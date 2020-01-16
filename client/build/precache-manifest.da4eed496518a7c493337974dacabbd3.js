ea9726b327d317aecc13c98dd48e1a48212b3e53
vitya752 <vitya752@ukr.net>
2020-01-16 12:22:30 +0200

    update build

---
+++ client/build/precache-manifest.da4eed496518a7c493337974dacabbd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39594c3230c0281bfb4b625d40f2fd1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92311ae085e2a4cc8ee8",
+    "url": "/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "b95b6b7aaa2f5c68dacd",
+    "url": "/static/css/main.da144858.chunk.css"
+  },
+  {
+    "revision": "92311ae085e2a4cc8ee8",
+    "url": "/static/js/2.5b80034b.chunk.js"
+  },
+  {
+    "revision": "a927c60a5fd43081aa8fe6f0ab8e74a3",
+    "url": "/static/js/2.5b80034b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b95b6b7aaa2f5c68dacd",
+    "url": "/static/js/main.c8bbc6ba.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  }
+]);
